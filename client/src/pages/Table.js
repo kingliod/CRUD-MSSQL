bd790b15dc7dbc73a4d6add6adbd4b28f47d61ce
kingliod <johnlloydc90@gmail.com>
2023-04-05 16:16:49 +0800

    s

--- client/src/pages/Table.js
+++ client/src/pages/Table.js
@@ -16,8 +16,7 @@ function createData(name, calories, fat, carbs, protein) {
 //   createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
 //   cr eateData("Eclair", 262, 16.0, 24, 6.0),
 //   createData("Cupcake", 305, 3.7, 67, 4.3),
-//   createData("Gingerbread", 356, 16.0, 49, 3.9),
-// ];/////
+// create Data
 //sowsws
 export default function BasicTable(props) {
   const { rows } = props;
